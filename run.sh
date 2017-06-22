d9071e7ca5f790db12e3fa23e92048df58e5512e
tahirstamboli <tahir.tamboli@reancloud.com>
2017-06-22 15:29:57 +0530

    sudo added to install inspec gem

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ echo "Running Actual inspec automation code using run.sh"
 #echo "password : ${SPEC_PASSWORD}"
 
 echo "installing inspec gem"
-gem install inspec
+sudo gem install inspec
 
 inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
 #inspec exec inspec-workshop --backend "winrm" --host "${SPEC_HOST_NAME}" --user "${SPEC_USER}" --password "${SPEC_PASSWORD}" --format html >> results.html
