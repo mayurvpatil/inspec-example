cb9556bdb36021b0932748bec6187164508e029f
pravkhande <preetika.ravkhande@reancloud.com>
2017-09-08 17:16:41 +0530

    inspec installed with bundler

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-echo "run.sh triggered by REAN Test"
-echo "Running Actual inspec automation code using run.sh"
-#echo "user : ${SPEC_USER}"
-#echo "host : ${SPEC_HOST_NAME}"
-#echo "password : ${SPEC_PASSWORD}"
-
-echo "installing inspec gem"
-sudo gem install inspec
-
+bundle install &&
 inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-#inspec exec inspec-workshop --backend "winrm" --host "${SPEC_HOST_NAME}" --user "${SPEC_USER}" --password "${SPEC_PASSWORD}" --format html >> results.html
-#inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json >> report/results.json
 inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
 
