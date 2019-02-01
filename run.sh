1fc5976ea7165900238fd129f9df59cd1537db0c
Rajashri Dalavi <rajashri.dalavi@reancloud.com>
2019-02-01 19:29:35 +0530

    update run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,11 @@
 #!/bin/sh
 
-# bundle install &&
-# inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-# inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
-
-
-
-#!/bin/sh
 
 # unzip vendor.zip
 # if [[ ! -e .bundle ]]; then
 #             mkdir .bundle
 # fi
 
-#This block is used for machine details from file
-
-#IFS=$'\n' GLOBIGNORE='*' command eval  'IPs=($(cat data/parameters.txt))'
-#
-#
-#for i in "${IPs[@]}"
-#do
-#        str=$i
-#        IFS=':' read -ra ary <<< "$str"
-#
-#        echo ${ary[0]}
-#        echo ${ary[1]}
-#        echo ${ary[2]}
-#        echo ${ary[3]}
-#        export SPEC_AUTHENTICATION=${ary[0]}
-#        export SPEC_IP=${ary[1]}
-#        export SPEC_USER=${ary[2]}
-#        export SPEC_PASSWORD=${ary[3]}
-#        nohup bash run_testnow_spec.sh  >> out.txt 2>&1 &
-#
-#done
 
 #This block is used for machine details from environment variable
 IFS=$';' GLOBIGNORE='*' command eval  'IPs=($IPS)'
@@ -72,7 +44,7 @@ do
             chmod 400 key/${SPEC_USER}_${SPEC_IP}.key
             fi
 
-            bundle install && inspec exec . -t ssh://${SPEC_USER}@${SPEC_IP} --key-files key/${SPEC_USER}_${SPEC_IP}.key --format json-rspec >> reports/results.json #--reporter json:./inspec_report.json html:./inspec_report.html #  bundle exec rake spec:"$HOST_ROLE" TITLE="${TITLE}"
+            bundle install && inspec exec . -t ssh://${SPEC_USER}@${SPEC_IP} --key-files key/${SPEC_USER}_${SPEC_IP}.key --format json-rspec >> reports/results.json && inspec exec . -t ssh://${SPEC_USER}@${SPEC_IP} --key-files key/${SPEC_USER}_${SPEC_IP}.key --format html >> report/results.html #--reporter json:./inspec_report.json html:./inspec_report.html #  bundle exec rake spec:"$HOST_ROLE" TITLE="${TITLE}"
             testExit=$?
             echo "-------------"
             # echo ${TITLE}
