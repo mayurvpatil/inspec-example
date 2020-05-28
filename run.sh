3b9c448a223444e787946102870953f0235276b0
aishwarya-mohite <aishwarya.mohite@reancloud.com>
2020-05-28 12:45:59 +0530

    testing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bundle install &&
-inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
+inspec exec inspec-workshop -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format html >> report/results.html
+inspec exec inspec-workshop -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format json-rspec >> report/results.json
 
