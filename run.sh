7db52dfefd0b7e1ce37708a3530cdb44929e5253
aishwarya-mohite <aishwarya.mohite@reancloud.com>
2020-05-29 13:24:57 +0530

    password
    Signed-off-by: aishwarya-mohite <aishwarya.mohite@reancloud.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bundle install &&
-inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format html >> reports/result.html
-inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format json-rspec >> reports/results.json
+inspec exec . -t winrm://${SERVERSPEC_USER}@${SERVERSPEC_IP} --password "${SERVERSPEC_PASSWORD}" --format html >> reports/result.html
+inspec exec . -t winrm://${SERVERSPEC_USER}@${SERVERSPEC_IP} --password "${SERVERSPEC_PASSWORD}" --format json-rspec >> reports/results.json
 
