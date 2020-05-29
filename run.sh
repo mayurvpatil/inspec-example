b80105daa54173bea7c1698b1bcd64f47dd5174e
aishwarya-mohite <aishwarya.mohite@reancloud.com>
2020-05-29 15:46:56 +0530

    inspec

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bundle install &&
-inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format html >> reports/result.html
-inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format json-rspec >> reports/results.json
+inspec exec . -t ssh://${INSPEC_USER}@${INSPEC_IP} --key-files "${INSPEC_SSH_KEY}" --format html >> reports/result.html
+inspec exec . -t ssh://${INSPEC_USER}@${INSPEC_IP} --key-files "${INSPEC_SSH_KEY}" --format json-rspec >> reports/results.json
 
