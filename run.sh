0be5222c8692d7aee10f93e7307bb14cbe69a68d
mayur patil <mayurvpatil>
2020-08-26 15:12:37 +0530

    reverted

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 bundle install &&
 inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format html >> reports/results.html
 inspec exec . -t ssh://${SERVERSPEC_USER}@${SERVERSPEC_IP} --key-files "${SERVERSPEC_SSH_KEY}" --format json-rspec >> reports/results.json
